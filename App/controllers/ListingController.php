da78a5224fd8fd750fea1150f7b7f0929f53ece7
Joel Persson <joelpersson@Joels-MacBook-Air.local>
2025-09-04 17:19:40 +0200

    Added flash message when deleting a listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -173,6 +173,9 @@ class ListingController
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
+        // Set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
+
         redirect('/listings');
     }
 }
