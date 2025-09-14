2ccfe95b1d7c938f80b5fcf51bd5eab4dacd72e5
Joel Persson <joelpersson@Joels-MacBook-Air.local>
2025-09-14 14:45:12 +0200

    refactored the flash message to functions in the session class rather then manual flash.

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -148,6 +148,8 @@ class ListingController
 
             $this->db->query($query, $newListingData);
 
+            Session::setFlashMessage('success_message', 'Listing created successfully');
+
             redirect('/listings');
         }
     }
@@ -176,14 +178,16 @@ class ListingController
 
         // Authorization
         if (!Authorization::isOwner($listing->user_id)) {
-            $_SESSION['error_message'] = 'You are not authorized to delete this listing';
+
+            Session::setFlashMessage('error_message', 'You are not authorized to delete this listing');
             return redirect('/listings/' . $listing->id);
         }
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
         // Set flash message
-        $_SESSION['success_message'] = 'Listing deleted successfully';
+        Session::setFlashMessage('success_message', 'Listing deleted successfully');
+
 
         redirect('/listings');
     }
@@ -292,12 +296,9 @@ class ListingController
             $updateValues['id'] = $id;
 
 
-            // inspectAndDie($updateValues);
-
-
             $this->db->query($updateQuery, $updateValues);
 
-            $_SESSION['success_message'] = 'Listing updated';
+            Session::setFlashMessage('success_message', 'Listing updated successfully');
 
             redirect('/listings/' . $id);
         }
